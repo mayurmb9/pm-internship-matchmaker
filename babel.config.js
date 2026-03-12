1e8b68f615f464b1397f129f4c63aa3113eea2e4
Mayur Borse <mayurborse16.netwin@yahoo.com>
2026-03-12 18:10:21 +0530

    fix: Resolve .env resolution errors and clean up imports

--- babel.config.js
+++ babel.config.js
@@ -2,12 +2,8 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     ['module:react-native-dotenv', {
-      "moduleName": "@env",
-      "path": ".env",
-      "blacklist": null,
-      "whitelist": null,
-      "safe": false,
-      "allowUndefined": true
+      moduleName: '@env',
+      path: '.env',
     }],
     'react-native-reanimated/plugin',
   ],
